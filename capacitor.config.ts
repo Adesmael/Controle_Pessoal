16eee4bf9fe650372daa6f22caf3545379995da4
Adilio Ismael dos Santos <AdilioFkis@gmail.com>
2025-07-16 20:44:14 +0000

    Atualiza workflow do Codemagic para build iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'controlePessoal.com.app',
+  appName: 'ControlePessoal',
+  webDir: 'public'
+};
+
+export default config;
