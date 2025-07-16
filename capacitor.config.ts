73c07fcab3f68aada166ea1cfc25c9f46af351ad
Adilio Ismael dos Santos <AdilioFkis@gmail.com>
2025-07-16 21:02:10 +0000

    Preciso compilar meu sistema pelo codemagic para gerar um app para insta

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,10 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'controlePessoal.com.app',
-  appName: 'ControlePessoal',
-  webDir: 'public'
+  appId: 'com.fluxofinanceiro.app',
+  appName: 'Fluxo Financeiro',
+  webDir: 'out', // Corrigido para a pasta de saída do Next.js estático
+  bundledWebRuntime: false,
 };
 
 export default config;
